381db3647e01a1ef6ebed6c88b68de0e3bc0e6b2
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-09 14:09:48 +0500

    Add-Migration

---
+++ src/Web.Infrastructure/Migrations/20230309090925_ChangeSeedData.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppMVC.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "TotalPrice",
+                value: 4889.9400000000005);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "TotalPrice",
+                value: 23377.175999999999);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "TotalPrice",
+                value: 4530.0479999999989);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Salt",
+                value: "1571e05c-7598-4834-af0e-d96de8789c17");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Salt",
+                value: "a53036de-822b-43f0-b43d-bb67fe0128a5");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "TotalPrice",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "TotalPrice",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "TotalPrice",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Salt",
+                value: "83f2aa34-24d2-4012-94ee-463f46e54078");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Salt",
+                value: "b5a4a53a-2477-47e3-898f-5563fd4a0d56");
+        }
+    }
+}
