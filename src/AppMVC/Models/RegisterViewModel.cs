c36d138d185307c77bcc5e3f9875287b7c7c7190
diyorbek2506@gmail.com <diyorbek2506@gmail.com>
2023-03-12 12:00:04 +0500

    added requered

--- src/AppMVC/Models/RegisterViewModel.cs
+++ src/AppMVC/Models/RegisterViewModel.cs
@@ -4,12 +4,15 @@ namespace AppMVC.Models;
 
 public class RegisterViewModel
 {
+    [Required(ErrorMessage = "Username is required")]
     [MaxLength(20, ErrorMessage = "Username cannot exceed 20 characters")]
     public string UserName { get; set; }
 
+    [Required(ErrorMessage = "Password is required")]
     [MaxLength(30, ErrorMessage = "Password cannot exceed 30 characters")]
     public string Password { get; set; }
 
+    [Required(ErrorMessage = "Password is required")]
     [MaxLength(30, ErrorMessage = "Password cannot exceed 30 characters")]
     public string PasswordVerify { get; set; }
 }
